4e120e76f56f5c1e15a33ecacb1abe7a5b5e8e09
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-03 15:15:36 +0500

    RSI indicator cannot be plotted easily using matplotlib

--- Multiple Linear Regression/load_dataset.py
+++ Multiple Linear Regression/load_dataset.py
@@ -4,4 +4,14 @@ import matplotlib.pyplot as plt
 file_path = r'C:\Users\Personal\Documents\AI Forex trading project and training\Multiple Linear Regression\EURUSDM1.csv'
 dataset = pd.read_csv(file_path)
 
-print(dataset.head())
+plt.figure(figsize=(12,6))
+plt.plot(dataset.index, dataset['open'], label='open', color='blue', marker='o')
+plt.plot(dataset.index, dataset['high'], marker='o', label='high', color='green')
+plt.plot(dataset.index, dataset['low'], label='low', color='red', marker='o')
+plt.plot(dataset.index, dataset['close'], label='clsoe', color='black', marker='o')
+plt.grid(True)
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.title('Forex Trading Data')
+plt.show()
