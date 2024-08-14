e9143858823998ecab0320f994c2c3d57cd4bcc4
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-14 11:19:48 +0500

    Reddit forex trading data scrapping using it API

--- NLP/Text Data Ingestion/reddit_data.py
+++ NLP/Text Data Ingestion/reddit_data.py
@@ -1,10 +1,31 @@
 import praw
+import json
 
 reddit = praw.Reddit(
-    client_id="8oArpdIMhn4f-TDsPcBsAw",
-    client_secret="I38X6Y3Blck7Xg1ZVttr0bB10WBAYw",
-    password="@Reddit.Pro",
-    user_agent="Ahmed255313",
-    username="scrap",
+    client_id="9_KhWJ0ZPJnCUVDa5iVClw",
+    client_secret="CYYiUFBNZrivlP0ig4v3j_IDt-PqoA",
+    user_agent="Beneficial-Visit-507",
+    username="scrapper",
 )
 
+subreddit = reddit.subreddit("Forex")
+posts_data = []
+
+for post in subreddit.top(time_filter="all", limit=100):
+    post_data = {
+        "title": post.title,
+        "description": post.selftext,
+        "url": post.url,
+        "comments": []
+    }
+
+    for comment in post.comments:
+        if isinstance(comment, praw.models.MoreComments):
+            continue
+        post_data["comments"].append(comment.body)
+
+    posts_data.append(post_data)
+
+# Save the post data to a JSON file
+with open("forex_posts.json", "w") as f:
+    json.dump(posts_data, f, indent=4)
