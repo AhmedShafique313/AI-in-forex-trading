e4f6112008cc511108659c0293652836266f3b92
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-14 10:27:04 +0500

    Reddit information is perfectly added

---
+++ NLP/Text Data Ingestion/reddit_data.py
@@ -0,0 +1,10 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="8oArpdIMhn4f-TDsPcBsAw",
+    client_secret="I38X6Y3Blck7Xg1ZVttr0bB10WBAYw",
+    password="@Reddit.Pro",
+    user_agent="Ahmed255313",
+    username="scrap",
+)
+
