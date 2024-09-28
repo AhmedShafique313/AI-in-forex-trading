0065c6a701e78939f463ba56048c640c3099a363
Haiqa Qaiser <2022ee255@student.uet.edu.pk>
2024-09-28 22:41:28 +0500

    model is working

---
+++ LSTM models/LSTM_model.py
@@ -0,0 +1,14 @@
+from scaling import *
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+# building model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 6)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test))
+mse = model.evaluate(x_test, y_test)
+print(f'MSE: {mse}')
