48c180667d98ef8ef43af49cec7ae103841d2def
Haiqa Qaiser <2022ee255@student.uet.edu.pk>
2024-09-28 21:31:58 +0500

    done

--- LSTM models/data_cleaning.py
+++ LSTM models/data_cleaning.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_csv(r'C:\Users\haiqa\OneDrive\Documents\Projects with Ahmed\Forex trading Project\AI-Forext-Trading-Project\Datasets\EURUSD_2024-01-01.csv', sep=',', header=None)
+df = pd.read_csv(r'C:\Users\haiqa\OneDrive\Documents\Projects with Ahmed\Forex trading Project\AI-Forext-Trading-Project\Datasets\30min\September 2024 30min dataset.csv', sep=',', header=None)
 df.columns = ['date', 'open', 'high', 'low', 'close']
 print(df)
