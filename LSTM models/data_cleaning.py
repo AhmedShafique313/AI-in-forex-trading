066332a19a9a36b70c7888a5714ec175570f1205
Haiqa Qaiser <2022ee255@student.uet.edu.pk>
2024-09-28 22:00:17 +0500

    data cleaning process for the LSTM is done

--- LSTM models/data_cleaning.py
+++ LSTM models/data_cleaning.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import numpy as np
 df = pd.read_csv(r'C:\Users\haiqa\OneDrive\Documents\Projects with Ahmed\Forex trading Project\AI-Forext-Trading-Project\Datasets\30min\September 2024 30min dataset.csv', sep=',', header=None)
 df.columns = ['date', 'open', 'high', 'low', 'close']
-print(df.head())
 df['date']= pd.to_datetime(df['date'])
 df['date_only']= df['date'].dt.date
 df['time_only']= df['date'].dt.time
 df.drop('date', axis=1, inplace=True)
 df.rename(columns={'date_only': 'date', 'time_only': 'time'}, inplace=True)
 print(df.head())
+print(df.tail())
+print(df.info())
+
+df['date'] = df['date'].apply(lambda x: x.toordinal())
+df['time'] = df['time'].apply(lambda x: x.hour + x.minute/60)
+print(df)
